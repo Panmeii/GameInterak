e466e766ecfd91ee29b7ff7c20aa2003734d3cb4
Panmeii <134782372+Panmeii@users.noreply.github.com>
2026-04-08 13:53:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+// Game Constants
+const SNOWMAN_COLUMNS = 4;
+const SNOWMAN_SPACE_COLUMN = 2.5;
+const SNOWMAN_SPACE_ROW = 4;
+const SCROLL_SPEED = 10;
+const GAMEBOARD_LENGTH = 56;
+
 import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+
+// Pastikan import sesuai struktur project kamu
+import { GameProvider } from "./context/GameProvider";
+import UI from "./components/UI";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <GameProvider>
+      <Canvas camera={{ position: [0, 8, 12], fov: 90 }}>
+        
+        {/* Fog */}
+        <fog attach="fog" args={["#333", 14, 35]} />
+
+        {/* Scene kamu di sini */}
+        
+
+        {/* Post Processing */}
+        <EffectComposer>
+          <Bloom
+            mipmapBlur
+            intensity={1.2}
+            luminanceThreshold={1}
+          />
+        </EffectComposer>
+
+      </Canvas>
+
+      {/* UI Overlay */}
+      <UI />
+    </GameProvider>
   );
 }
 
