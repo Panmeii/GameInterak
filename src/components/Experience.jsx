8bf40c1b7e26bbd1b353ac942eb436b187c8646e
Panmeii <134782372+Panmeii@users.noreply.github.com>
2026-04-08 14:03:15 +0700

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,60 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei"
+import { useGame } from "../hooks/useGame"
+import { GAMEBOARD_LENGTH } from "../App"
 
-export const Experience = () => {
+import Gameboard from "./Gameboard"
+import Snowman from "./Snowman"
+import Grave from "./Grave"
+import Santa from "./Santa"
+import Explosion from "./Explosion"
+import Final from "./Final"
+
+export default function Experience() {
+  const { state } = useGame()
+
+  const { status, snowmen, showBomb } = state
+
   return (
     <>
+      {/* Lighting */}
+      <Environment preset="sunset" />
+
+      <directionalLight
+        position={[5, 10, 5]}
+        intensity={1.5}
+        castShadow
+      />
+
+      {/* Controls (debug / dev) */}
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+
+      {/* Game Over Screen */}
+      {status === "gameover" && <Final />}
+
+      {/* Game Scene */}
+      {status === "playing" && (
+        <>
+          {/* Gameboards (loop illusion) */}
+          <Gameboard position={[0, 0, 0]} />
+          <Gameboard position={[0, 0, 42]} />
+          <Gameboard position={[0, 0, -GAMEBOARD_LENGTH]} />
+
+          {/* Snowmen */}
+          {snowmen.map((snowman) =>
+            snowman.hp > 0 ? (
+              <Snowman key={snowman.id} {...snowman} />
+            ) : (
+              <Grave key={snowman.id} {...snowman} />
+            )
+          )}
+
+          {/* Bomb Effect */}
+          {showBomb && <Explosion />}
+
+          {/* Player */}
+          <Santa position={[0, 0, 6]} />
+        </>
+      )}
     </>
-  );
-};
+  )
+}
